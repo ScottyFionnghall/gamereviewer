a50925279980a910ae0077f53c254eef0e32a970
Alexey Antipin <scottyfionnghall@gmail.com>
2022-11-30 03:32:22 +0300

    small comment changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import steam_score
 import random
 # update game list
-steam_score.updateGamesList()
+# steam_score.updateGamesList()
 name = input().strip('  ').lower()
 # get review score 
 score = steam_score.getReviewScore(steam_score.getAppID(name))
 # list of reviews
 review = {1:'terrible',2:'bad game',3:'meh',4:'good game',5:'masterpiece'}
 if score:
-    # make review score range from 0 to 10 to 1 to 5
+    # make review score range from 0-10 to 1-5
     score = round(score*0.6)
     # 25% of the time script will give out opposite review
     if random.randint(0,100) <= 25:
